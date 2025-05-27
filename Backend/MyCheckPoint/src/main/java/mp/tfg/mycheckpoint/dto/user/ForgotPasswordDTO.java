2d1df1c125b1d6d32de793788d8322817701e5c6
mfercob430 <mfercob430@g.educaand.es>
2025-05-27 12:35:49 +0200

    Documentado ForgotPasswordDTO

--- Backend/MyCheckPoint/src/main/java/mp/tfg/mycheckpoint/dto/user/ForgotPasswordDTO.java
+++ Backend/MyCheckPoint/src/main/java/mp/tfg/mycheckpoint/dto/user/ForgotPasswordDTO.java
@@ -1,14 +1,18 @@
 package mp.tfg.mycheckpoint.dto.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "DTO para la solicitud de restablecimiento de contraseña. Requiere el correo electrónico asociado a la cuenta.")
 @Data
 @NoArgsConstructor
 public class ForgotPasswordDTO {
+    @Schema(description = "Correo electrónico del usuario que solicita el restablecimiento de contraseña. Debe ser un formato de email válido.",
+            example = "usuario@example.com", requiredMode = Schema.RequiredMode.REQUIRED, format = "email")
     @NotBlank(message = "El email no puede estar vacío.")
     @Email(message = "El formato del email no es válido.")
     @JsonProperty("email")
